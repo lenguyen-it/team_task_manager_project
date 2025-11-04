83ac12569475ed09e3587efc0631b739337f6f18
lenguyen-it <lethainguyenit2002@gmail.com>
2025-11-04 14:48:19 +0700

    Thêm url cho thiết bị thật, nhưng sử dụng ở local

--- task_team_frontend_mobile/lib/config/env.dart
+++ task_team_frontend_mobile/lib/config/env.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-
 class Env {
   static String get baseUrl => dotenv.env['API_BASE_URL'] ?? '';
+  static String get localUrl => dotenv.env['API_LOCAL_DEVICE_URL'] ?? '';
 }
