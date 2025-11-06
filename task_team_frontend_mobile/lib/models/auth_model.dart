ba581813eb936c3b82394a68e16c010e3b1c5a9c
lenguyen-it <lethainguyenit2002@gmail.com>
2025-11-06 16:07:37 +0700

    model cho auth để quản lý token

---
+++ task_team_frontend_mobile/lib/models/auth_model.dart
@@ -0,0 +1,111 @@
+import 'package:task_team_frontend_mobile/models/employee_model.dart';
+
+class LoginRequest {
+  final String employeeId;
+  final String employeePassword;
+
+  LoginRequest({
+    required this.employeeId,
+    required this.employeePassword,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'employee_id': employeeId,
+      'employee_password': employeePassword,
+    };
+  }
+}
+
+class RegisterRequest {
+  final String employeeId;
+  final String employeeName;
+  final String employeePassword;
+  final String roleId;
+  final String email;
+  final String? phone;
+  final String? image;
+
+  RegisterRequest({
+    required this.employeeId,
+    required this.employeeName,
+    required this.employeePassword,
+    required this.roleId,
+    required this.email,
+    this.phone,
+    this.image,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'employee_id': employeeId,
+      'employee_name': employeeName,
+      'employee_password': employeePassword,
+      'role_id': roleId,
+      'email': email,
+      'phone': phone,
+      'image': image,
+    };
+  }
+}
+
+class LoginResponse {
+  final String token;
+  final EmployeeInfo employee;
+
+  LoginResponse({
+    required this.token,
+    required this.employee,
+  });
+
+  factory LoginResponse.fromJson(Map<String, dynamic> json) {
+    return LoginResponse(
+      token: json['token'],
+      employee: EmployeeInfo.fromJson(json['employee']),
+    );
+  }
+}
+
+class EmployeeInfo {
+  final String employeeId;
+  final String roleId;
+
+  EmployeeInfo({
+    required this.employeeId,
+    required this.roleId,
+  });
+
+  factory EmployeeInfo.fromJson(Map<String, dynamic> json) {
+    return EmployeeInfo(
+      employeeId: json['employee_id'],
+      roleId: json['role_id'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'employee_id': employeeId,
+      'role_id': roleId,
+    };
+  }
+}
+
+class RegisterResponse {
+  final String message;
+  final String employeeName;
+  final EmployeeModel employee;
+
+  RegisterResponse({
+    required this.message,
+    required this.employeeName,
+    required this.employee,
+  });
+
+  factory RegisterResponse.fromJson(Map<String, dynamic> json) {
+    return RegisterResponse(
+      message: json['message'],
+      employeeName: json['employee_name'],
+      employee: EmployeeModel.fromJson(json['employee']),
+    );
+  }
+}
